b2c0285031ddcce38b471238028d564420805ac4
Dima Aseev <dmiseev@gmail.com>
2018-03-27 19:43:30 +0300

    Added tests

---
+++ src/test/home.ts
@@ -0,0 +1,12 @@
+import 'mocha';
+
+import { expect } from 'chai';
+import { HomeController} from '../Http/Controllers/HomeController';
+
+describe('HomeController', () => {
+    it('should give back `Home sweet home`', () => {
+        let service = new HomeController();
+
+        expect(service.home()).to.equal('Home sweet home');
+    });
+});
