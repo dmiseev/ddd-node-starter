cf95f7983f4dab499a83d21ad79bc94de2996368
2dmaster <dmasterfake@gmail.com>
2018-04-06 11:30:08 +0300

    custom request type fix, nodemon compilation free dev configuration

--- src/Utils/Request/custom.ts
+++ src/Utils/Request/custom.ts
@@ -1,7 +1,7 @@
 import { User } from '../../Domain/User/User';
 import * as express from 'express';
 
-interface IRequest extends express.Request {
+export interface IRequest extends express.Request {
     user?: User
     file?: any
 }
